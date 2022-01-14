be00aee66485709b8b2d44336af10730cf9c41c9
Richard2415 <richardselvaraj07@gmail.com>
2022-01-14 17:12:16 +0530

    make it better

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config()
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/Driver",
+  process.env.MONGODB_URI || "mongodb://localhost/truck-driver-app",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
